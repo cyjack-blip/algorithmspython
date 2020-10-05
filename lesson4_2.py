de29b455019278b108cf295d78b412a9d64915fd
cyjack-blip <deneb@dfds.ru>
2020-10-05 03:33:11 +0300

    Урок 4

--- lesson4_2.py
+++ lesson4_2.py
@@ -1,6 +1,18 @@
+# Задание 2
+#
+# Написать два алгоритма нахождения i-го по счёту простого числа.
+# Функция нахождения простого числа должна принимать на вход натуральное и возвращать соответствующее простое число.
+# Проанализировать скорость и сложность алгоритмов.
+# Первый — с помощью алгоритма «Решето Эратосфена».
+# Примечание. Алгоритм «Решето Эратосфена» разбирался на одном из прошлых уроков.
+# Используйте этот код и попробуйте его улучшить/оптимизировать под задачу.
+# Второй — без использования «Решета Эратосфена».
 #
 # Внимание! В программе первое простое число считается = 2
 #
+#  для того чтобы получить n-ое число, надо определить какого размера массив чисел просеивать через решето
+#  нужное нам n-ое простое число будет оценочно не больше max
+# max = int(n * (math.log(n) + math.log(math.log(n)) - 0.5))
 
 import functools
 import math
@@ -35,8 +47,14 @@ def sieve(n):
 # "lesson4_2.sieve(10)" 1000 loops, best of 5: 6.86 usec per loop
 # "lesson4_2.sieve(100)" 1000 loops, best of 5: 51.8 usec per loop
 # "lesson4_2.sieve(1000)" 1000 loops, best of 5: 972 usec per loop
-#  "lesson4_2.sieve(5000)" 1000 loops, best of 5: 6.35 msec per loop
+# "lesson4_2.sieve(5000)" 1000 loops, best of 5: 6.35 msec per loop
 
+# cProfile.run('sieve(10)'), 26 function calls in 0.000 seconds
+# cProfile.run('sieve(100)'), 110 function calls in 0.000 seconds
+# cProfile.run('sieve(1000)'), 1052 function calls in 0.001 seconds
+# cProfile.run('sieve(5000)'), 5211 function calls in 0.010 seconds
+
+# сложность алгоритма линейная O(n)
 
 def get_n_prime(a):
     """получает простое число перебором"""
@@ -59,12 +77,14 @@ def get_n_prime(a):
 # "lesson4_2.get_n_prime(10)" 1000 loops, best of 5: 21.1 usec per loop
 # "lesson4_2.get_n_prime(100)" 1000 loops, best of 5: 1.2 msec per loop
 # "lesson4_2.get_n_prime(1000)" 1000 loops, best of 5: 186 msec per loop
-#  "lesson4_2.get_n_prime(5000)" 100 loops, best of 5: 6.01 sec per loop
+# "lesson4_2.get_n_prime(5000)" 100 loops, best of 5: 6.01 sec per loop
 
 # cProfile.run('get_n_prime(10)') 49 function calls in 0.000 seconds
 # cProfile.run('get_n_prime(100)') 561 function calls in 0.001 seconds
 # cProfile.run('get_n_prime(1000)') 7939 function calls in 0.201 seconds
 # cProfile.run('get_n_prime(5000)') 48631 function calls in 6.255 seconds
+
+# сложность алгоритма логарифмическая O(log n)
 
 def prime_7(n):
     """ вариант решета """
@@ -90,19 +110,19 @@ def prime_7(n):
 # "lesson4_2.prime_7(1000)" 1000 loops, best of 5: 3.3 msec per loop
 # "lesson4_2.prime_7(5000)" 1000 loops, best of 5: 30.7 msec per loop
 
+# cProfile.run('prime_7(10)'), 26 function calls in 0.000 seconds
+# cProfile.run('prime_7(100)'), 110 function calls in 0.000 seconds
+# cProfile.run('prime_7(1000)'), 1052 function calls in 0.004 seconds
+# cProfile.run('prime_7(5000)'), 5211 function calls in 0.037 seconds
+
+# сложность алгоритма линейная O(n)
+
 
 n = 100
-
-#  для того чтобы получить n-ое число, надо определить какого размера массив чисел просеивать через решето
-#  нужное нам n-ое простое число будет оценочно не больше max
-# max = int(n * (math.log(n) + math.log(math.log(n)) - 0.5))
-
-# print(f'N-th prime is less then {max}')
 
 # print(f'Result by sieve {sieve(n)}')
 # print(f'Result by ya_prime {prime_7(n)}')
 # print(f'Result by bruteforce {get_n_prime(n)}')
 
-
-#cProfile.run('get_n_prime(10)')
+# cProfile.run('prime_7(5000)')
 
